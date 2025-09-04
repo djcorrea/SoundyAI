fbed3f23967f6e0c643c7b6f38fee18fe009b5b6
DJ Correa <djcorreamg@gmail.com>
2025-09-04 20:47:50 -0300

    rota

--- server.js
+++ server.js
@@ -33,3 +33,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("✅ Servidor do SoundyAI rodando no Railway!");
+});
